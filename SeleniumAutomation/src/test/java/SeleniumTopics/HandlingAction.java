4cf10b208fdac563a9b8ca3405ea47d49348c07d
prave <prave@Praveena>
2026-01-23 14:13:07 +0530

    23-01-2026

--- SeleniumAutomation/src/test/java/SeleniumTopics/HandlingAction.java
+++ SeleniumAutomation/src/test/java/SeleniumTopics/HandlingAction.java
@@ -1,6 +1,10 @@
 package SeleniumTopics;
 
+import java.awt.AWTException;
 import java.awt.Desktop.Action;
+import java.awt.RenderingHints.Key;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -12,14 +16,47 @@ public class HandlingAction extends BaseBasic
 	{
 		WebElement others=driver.findElement(By.xpath("//a[@id='others']"));
 		Actions action=new Actions(driver);
-		action.contextClick().build().perform();
+		action.contextClick(others).build().perform();
 	}
+	public void verifyMouseHover()
+	{
+		WebElement others=driver.findElement(By.xpath("//a[@id='others']"));
+		Actions action=new Actions(driver);
+		action.moveToElement(others).build().perform();
+	}
+	public void verifyDragDrop()
+	{  driver.navigate().to("https://demoqa.com/droppable");
+		WebElement drag=driver.findElement(By.xpath("//div[@id='draggable']"));
+		WebElement drop=driver.findElement(By.xpath("//div[@id='droppable']"));
+		Actions action=new Actions(driver);
+		action.dragAndDrop(drag, drop).build().perform();
+		
+		
+	}
+	public void verifyKeyBoard() throws AWTException
+	{
+	 Robot robot=new Robot();
+	 robot.keyPress(KeyEvent.VK_CONTROL);
+	 robot.keyPress(KeyEvent.VK_T);
+	 robot.keyRelease(KeyEvent.VK_CONTROL);
+	 robot.keyRelease(KeyEvent.VK_T);
+	}
+	
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HandlingAction actions=new HandlingAction();
 		actions.intializeBrowser();
-		actions.verifyRightClick();
+		//actions.verifyRightClick();
+		//actions.verifyMouseHover();
+		actions.verifyDragDrop();
+		//try {
+		//	actions.verifyKeyBoard();
+		//} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		//}
 	}
 
 }
